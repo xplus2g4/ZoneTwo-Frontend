e52730ce5756a7a111efdaa644c5571378de901c
xplus2g4 <xplus2g4@gmail.com>
2024-08-13 11:12:44 +0800

    Add music overview tests

--- integration_test/music_download_page_test.dart
+++ integration_test/music_download_page_test.dart
@@ -4,10 +4,30 @@ import 'package:integration_test/integration_test.dart';
 
 import 'mocks/fake_app.dart';
 
-Future<void> _loadDownloadMusicPage(WidgetTester tester) async {
+Future<void> loadDownloadMusicPage(WidgetTester tester) async {
   await tester.pumpWidget(await fakeApp());
   final addMusicBtn = find.text('Add Music');
   await tester.tap(addMusicBtn);
+  await tester.pumpAndSettle();
+}
+
+Future<void> downloadMusic(WidgetTester tester) async {
+  // Go to music download page from music overview page
+  await loadDownloadMusicPage(tester);
+
+  // Enter Youtube URL
+  await tester.enterText(
+    find.byType(TextField),
+    'https://www.youtube.com/watch?v=zGDzdps75ns',
+  );
+  await tester.testTextInput.receiveAction(TextInputAction.done);
+
+  // Wait for download to complete
+  await Future.delayed(const Duration(seconds: 5));
+
+  // Go back to music overview page
+  final NavigatorState navigator = tester.state(find.byType(Navigator).last);
+  navigator.pop();
   await tester.pumpAndSettle();
 }
 
@@ -16,7 +36,7 @@ void main() {
 
   group("music download page test", () {
     testWidgets("navigating to page", (tester) async {
-      await _loadDownloadMusicPage(tester);
+      await loadDownloadMusicPage(tester);
 
       expect(find.text("Download Music"), findsOneWidget);
     });
@@ -30,8 +50,10 @@ void main() {
       await tester.testTextInput.receiveAction(TextInputAction.done);
       await tester.pumpAndSettle();
       expect(find.text("zGDzdps75ns"), findsOneWidget);
+
       await Future.delayed(const Duration(seconds: 10));
       await tester.pumpAndSettle();
+
       expect(find.text("Small short test video.mp3"), findsOneWidget);
     });
   });
--- integration_test/music_overview_page_test.dart
+++ integration_test/music_overview_page_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 import 'mocks/fake_app.dart';
+import 'music_download_page_test.dart';
 
 Future<void> _loadMusicOverviewPage(WidgetTester tester) async {
   await tester.pumpWidget(await fakeApp());
@@ -22,6 +23,13 @@ void main() {
       await _loadMusicOverviewPage(tester);
 
       expect(find.text("Add your music now!"), findsOneWidget);
+    });
+
+    testWidgets("page with music", (tester) async {
+      await downloadMusic(tester);
+
+      expect(find.text("Add your music now!"), findsNothing);
+      expect(find.text("Small short test video"), findsOneWidget);
     });
   });
 }
